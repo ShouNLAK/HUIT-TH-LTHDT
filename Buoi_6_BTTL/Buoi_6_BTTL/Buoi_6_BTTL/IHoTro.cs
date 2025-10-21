9c1ec9eea20d1192484ec355880b6d98c4f4f897
Nguyen Le Anh Khoa <shounlak@gmail.com>
2025-10-21 16:22:58 +0700

    Add files via upload

---
+++ Buoi_6_BTTL/Buoi_6_BTTL/Buoi_6_BTTL/IHoTro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi_6_BTTL
+{
+     interface IHoTro
+    {
+        double PhiKinhDoanh();
+    }
+}
